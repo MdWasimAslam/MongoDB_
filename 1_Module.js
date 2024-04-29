5e12ae0d36a855642a8835979bd1e2da22d4411f
Md Wasim Aslam <wasimaslam2897@gmail.com>
2024-04-30 02:42:04 +0530

    update

--- 1_Module.js
+++ 1_Module.js
@@ -54,5 +54,74 @@
   ]
 
 
+  // Find total number of males and females
+  [
+    {
+      $group: {
+        _id: "$gender",
+        count:{
+          $sum:1
+        }
+      }
+    }
+  ]
+
+
+  // Which country has the highest number of registered users
+  [
+    {
+      $group: {
+        _id: "$company.location.country",
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $sort: {
+        count: -1
+      }
+    },
+    {
+      $limit: 1
+    }
+  ]
+
+
+  // Count number of users based on eyeColor
+  [
+    {
+      $group: {
+        _id: "$eyeColor",
+          count:{
+            $sum:1
+          }
+      }
+    }
+  ]
+
+
+  // what is the average number of tags per user
+  [
+    {
+      $unwind: '$tags'
+    },
+    {
+      $group: {
+        _id: "$_id",
+        tagsCount: {
+          $sum:1
+        }
+      }
+    },
+    {
+      $group: {
+        _id: null,
+        avg: {
+          $avg: "$tagsCount"
+        }
+      }
+    }
+  ]
 
   
