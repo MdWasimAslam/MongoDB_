56efca943a3a2831f8f6298c1c927d28d14d8f85
Md Wasim Aslam <wasimaslam2897@gmail.com>
2024-05-05 15:01:33 +0530

    update mongoose

---
+++ Mongoose_WDS/index.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const users = require('./Schema/users');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect('mongodb://localhost:27017/mongoosedb', { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+        // Call the createUser function after the connection is established
+        await mongoAggregate();
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+connectDB();
+
+const mongoQuery = async () => {
+    try {
+        const db = mongoose.connection.db;
+        const res = await db.collection('users').insertOne({name:'rahul',age:21});
+        console.log(res);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+const mongoAggregate = async () => {
+    try {
+        const db = mongoose.connection.db;
+        const aggregatePipeline = [
+            {
+              $match: {
+                name:'Kyle'
+              }
+            }
+          ]
+        const res = await db.collection('users').aggregate(aggregatePipeline).toArray();
+        console.log(res);
+    } catch (err) {
+        console.error(err);
+    }
+}
---
+++ Mongoose_WDS/sampleData.js
@@ -0,0 +1,183 @@
+db.students.insertMany([
+    {
+      "_id": 4,
+      "name": "David",
+      "age": 19,
+      "gender": "male",
+      "major": "Mathematics",
+      "grades": {
+        "math": 95,
+        "physics": 88,
+        "chemistry": 85
+      },
+      "address": {
+        "street": "321 Pine St",
+        "city": "Mountainville",
+        "state": "WA",
+        "zipcode": "98765"
+      }
+    },
+    {
+      "_id": 5,
+      "name": "Emma",
+      "age": 20,
+      "gender": "female",
+      "major": "Psychology",
+      "grades": {
+        "math": 78,
+        "physics": 70,
+        "chemistry": 82
+      },
+      "address": {
+        "street": "567 Cedar St",
+        "city": "Laketown",
+        "state": "FL",
+        "zipcode": "23456"
+      }
+    },
+    {
+      "_id": 6,
+      "name": "Frank",
+      "age": 23,
+      "gender": "male",
+      "major": "History",
+      "grades": {
+        "math": 82,
+        "physics": 75,
+        "chemistry": 80
+      },
+      "address": {
+        "street": "987 Maple St",
+        "city": "Riverdale",
+        "state": "GA",
+        "zipcode": "34567"
+      }
+    },
+    {
+      "_id": 7,
+      "name": "Grace",
+      "age": 22,
+      "gender": "female",
+      "major": "English Literature",
+      "grades": {
+        "math": 70,
+        "physics": 65,
+        "chemistry": 72
+      },
+      "address": {
+        "street": "654 Birch St",
+        "city": "Hillside",
+        "state": "NC",
+        "zipcode": "87654"
+      }
+    },
+    {
+      "_id": 8,
+      "name": "Henry",
+      "age": 21,
+      "gender": "male",
+      "major": "Economics",
+      "grades": {
+        "math": 88,
+        "physics": 82,
+        "chemistry": 85
+      },
+      "address": {
+        "street": "210 Walnut St",
+        "city": "Valleytown",
+        "state": "OR",
+        "zipcode": "45678"
+      }
+    },
+    {
+      "_id": 9,
+      "name": "Isabella",
+      "age": 20,
+      "gender": "female",
+      "major": "Sociology",
+      "grades": {
+        "math": 76,
+        "physics": 68,
+        "chemistry": 75
+      },
+      "address": {
+        "street": "876 Oak St",
+        "city": "Rivertown",
+        "state": "IL",
+        "zipcode": "76543"
+      }
+    },
+    {
+      "_id": 10,
+      "name": "Jack",
+      "age": 24,
+      "gender": "male",
+      "major": "Political Science",
+      "grades": {
+        "math": 85,
+        "physics": 78,
+        "chemistry": 82
+      },
+      "address": {
+        "street": "543 Cherry St",
+        "city": "Greenville",
+        "state": "SC",
+        "zipcode": "54321"
+      }
+    },
+    {
+      "_id": 11,
+      "name": "Katherine",
+      "age": 22,
+      "gender": "female",
+      "major": "Chemical Engineering",
+      "grades": {
+        "math": 90,
+        "physics": 85,
+        "chemistry": 92
+      },
+      "address": {
+        "street": "789 Pine St",
+        "city": "Forestville",
+        "state": "MA",
+        "zipcode": "34567"
+      }
+    },
+    {
+      "_id": 12,
+      "name": "Liam",
+      "age": 19,
+      "gender": "male",
+      "major": "Art",
+      "grades": {
+        "math": 72,
+        "physics": 65,
+        "chemistry": 68
+      },
+      "address": {
+        "street": "234 Elm St",
+        "city": "Harborview",
+        "state": "WA",
+        "zipcode": "67890"
+      }
+    },
+    {
+      "_id": 13,
+      "name": "Mia",
+      "age": 20,
+      "gender": "female",
+      "major": "Nursing",
+      "grades": {
+        "math": 85,
+        "physics": 78,
+        "chemistry": 82
+      },
+      "address": {
+        "street": "321 Maple St",
+        "city": "Seaside",
+        "state": "CA",
+        "zipcode": "12345"
+      }
+    }
+  ]
+  )
